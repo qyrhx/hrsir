e28913f898f0c4cea370a48beb63ce655e458f74
qyrhx <qyrhx@disroot.org>
2026-01-28 15:39:19 +0100

    Write and read config file

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main (main) where
+
+import System.Directory
+import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import GHC.Generics (Generic)
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+data Config = Config { feeds :: [String] } deriving (Show, Generic)
+
+instance FromJSON Config
+instance ToJSON Config
+
+configFile :: IO FilePath
+configFile = do
+  h <- getHomeDirectory
+  return $ h ++ "/.config/hrsir_data.json"
+
+getConfig :: FilePath -> IO (Maybe Config)
+getConfig path = do
+  txt <- BL.readFile path
+  return $ decode txt
+
+writeConfig :: FilePath -> Config -> IO ()
+writeConfig path config = do
+  BL.writeFile path $ encode config
+  return ()
 
 main :: IO ()
 main = putStrLn "yo..."
